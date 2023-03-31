6dae954fac712521cb1c7617f8f1206f5f356aab
dtidwell <conversations.and.L@gmail.com>
2023-03-31 15:26:23 -0500

    event lambda

---
+++ app/src/main/java/lambda/GetEventsLambda.java
@@ -0,0 +1,25 @@
+package lambda;
+
+
+import activity.requests.GetEventsRequest;
+import activity.requests.GetUserRequest;
+import activity.results.GetEventsResult;
+import activity.results.GetUserResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class GetEventsLambda
+        extends LambdaActivityRunner<GetEventsRequest, GetEventsResult>
+        implements RequestHandler<LambdaRequest<GetEventsRequest>, LambdaResponse> {
+    @Override
+    public LambdaResponse handleRequest(LambdaRequest<GetEventsRequest> input, Context context) {
+        return super.runActivity(
+                () -> input.fromPath(path ->
+                        GetEventsRequest.builder()
+                                .withId(path.get("id"))
+                                .build()),
+                (request, serviceComponent) ->
+                        serviceComponent.provideGetEventsActivity().handleRequest(request)
+        );
+    }
+}
